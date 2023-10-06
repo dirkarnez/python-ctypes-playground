a7227bc2554a811dde2ce55c370453e8111cb0f4
Dirk Arnez <smalldirkalex@gmail.com>
2023-10-06 21:25:33 +0800

    - add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from ctypes import *
+
+lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+lib.network_width.argtypes = [c_void_p]
+lib.network_width.restype = c_int
+lib.network_height.argtypes = [c_void_p]
+lib.network_height.restype = c_int
